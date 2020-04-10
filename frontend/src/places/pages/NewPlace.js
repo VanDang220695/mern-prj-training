b74304879f5c03583da435672e684625518a2a64
VanDang220695 <nguyenvandang22061995@gmail.com>
2020-04-10 15:37:53 +0700

    Implement Shared component

---
+++ frontend/src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewPlace = () => {
+  return <p>Newplace work!</p>;
+};
+
+export default NewPlace;
---
+++ frontend/src/user/components/UserList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import Card from '../../shared/components/UI/Card';
+
+import UserItem from './UserItem';
+
+import './UserList.css';
+
+const UserList = (props) => {
+  if (props.items.length === 0) {
+    return (
+      <div className='center '>
+        <Card>No users found</Card>
+      </div>
+    );
+  }
+  return (
+    <ul className='users-list'>
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
